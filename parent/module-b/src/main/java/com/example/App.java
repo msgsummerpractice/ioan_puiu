3522658e71dbed41f888210aa5c50560258af9f1
ioan alexandru <ionutpuiu010405@gmail.com>
2026-07-23 15:08:09 +0200

    Create build profiles, use inheritance

--- parent/module-b/src/main/java/com/example/App.java
+++ parent/module-b/src/main/java/com/example/App.java
@@ -4,10 +4,9 @@ package com.example;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+  public static void main(String[] args) {
+            System.out.println( "Hello World!" );
+    System.out.println("Environment: " + System.getProperty("env"));
+  }
 }
