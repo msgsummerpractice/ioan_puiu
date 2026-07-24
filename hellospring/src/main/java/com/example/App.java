76326aed7e55dccd30dee116450f9dcbaa6a2fe4
ioan alexandru <ionutpuiu010405@gmail.com>
2026-07-24 11:10:22 +0200

    update the config to scan the project by itself

--- hellospring/src/main/java/com/example/App.java
+++ hellospring/src/main/java/com/example/App.java
@@ -1,17 +1,14 @@
 package com.example;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-@Configuration
-@ComponentScan(basePackages = "com.example")
+
 public class App {
   
    public static void main(String[] args) {
 
-      ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
+      ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
       HelloWorld obj = (HelloWorld) context.getBean("helloWorld", HelloWorld.class);
       obj.setMessage("Hello Spring!");
       obj.getMessage();
