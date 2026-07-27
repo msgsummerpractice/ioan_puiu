dfc6536167ce5ce913600ffee1bd523825d4ee4c
ioan alexandru <ionutpuiu010405@gmail.com>
2026-07-27 14:04:57 +0200

    Finish model, service and repository using Lombok and JPARepository. Create tests for service

---
+++ DBdemo/src/main/java/com/example/DBdemo/Repository/UserRepository.java
@@ -0,0 +1,19 @@
+package com.example.DBdemo.Repository;
+
+import com.example.DBdemo.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
+
+    User findByEmail(String email);
+
+    User findById(long id);
+
+    
+
+
+}
---
+++ DBdemo/src/main/java/com/example/DBdemo/Service/UserService.java
@@ -0,0 +1,44 @@
+package com.example.DBdemo.Service;
+
+import com.example.DBdemo.Repository.UserRepository;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import com.example.DBdemo.Model.User;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User removeUser(User user) {
+        userRepository.delete(user);
+        return user;
+    }
+
+    public User getUserById(long id) {
+        return userRepository.findById(id);
+    }
+
+    public User getUserByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public User getUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+}
