cc4e6de3fb5c3fe0def14d2e60000cf0b90be13a
ioan alexandru <ionutpuiu010405@gmail.com>
2026-07-30 14:53:16 +0200

    Create second project. Finish exercise from 205

--- frontend/202-setup-ng/my-app/src/app/app.ts
+++ frontend/202-setup-ng/my-app/src/app/app.ts
@@ -1,8 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatButton } from '@angular/material/button';
 import { MatToolbar } from '@angular/material/toolbar';
 import { ComponentaMeaPuternica } from './componenta-mea-puternica/componenta-mea-puternica';
+import { dogscript } from './dogscript';
+import { forkJoin } from 'rxjs/internal/observable/forkJoin';
 
 @Component({
   selector: 'app-root',
@@ -12,4 +14,30 @@ import { ComponentaMeaPuternica } from './componenta-mea-puternica/componenta-me
 })
 export class App {
   protected readonly title = signal('my-app');
+  private dogScript = inject(dogscript);
+
+  dogImage = signal<string[]>([]);
+
+  loadDogs() {
+    const request = [
+      this.dogScript.getRandomDogImage(),
+      this.dogScript.getRandomDogImage(),
+      this.dogScript.getRandomDogImage(),
+    ];
+
+    forkJoin(request).subscribe((results) => {
+      const images = results.map((dog) => dog.message);
+      this.dogImage.set(images);
+    });
+  }
+
+  getRandomDogImage(index: number): void {
+    this.dogScript.getRandomDogImage().subscribe((response) => {
+      const images = this.dogImage();
+      images[index] = response.message;
+      this.dogImage.set(images);
+      console.log('Dog image updated at index', index, ':', response.message);
+      console.log('Current dog images:', this.dogImage());
+    });
+  }
 }
