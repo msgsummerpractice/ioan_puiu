f9b1a0fb7a2d654fec6a601bed82d4f97eda4f3c
ioan alexandru <ionutpuiu010405@gmail.com>
2026-07-30 16:12:02 +0200

    feat: Set routing up for /login, /home, /*anything* that routes toward notFoundComponent

--- frontend/202-setup-ng/my-app/src/app/app.routes.ts
+++ frontend/202-setup-ng/my-app/src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { provideRouter, Routes } from '@angular/router';
+import { HomeComponent } from './home-component/home-component';
+import { NotFoundComponent } from './not-found-component/not-found-component';
+import { App } from './app';
+import { bootstrapApplication } from '@angular/platform-browser';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'login',
+    loadComponent: () => import('./login-component/login-component').then((m) => m.LoginComponent),
+  },
+  { path: '**', component: NotFoundComponent },
+];
+
+bootstrapApplication(App, {
+  providers: [provideRouter(routes)],
+});
